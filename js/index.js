0dba5867cd4137e65d96e835c5c34b69df24f4ec
pia9322 <77098879+pia9322@users.noreply.github.com>
2021-07-30 09:57:37 +0900

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,81 @@
+$(function(){
+    AOS.init();
+
+    
+    var swiper = new Swiper('.swiper-container', {
+        effect: 'fade',
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: false,
+        },
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'fraction',
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        on: {
+            init: function () {
+                $('.swiper-progress-bar').removeClass('animate');
+                $('.swiper-progress-bar').removeClass('active');
+                $('.swiper-progress-bar').eq(0).addClass('animate');
+                $('.swiper-progress-bar').eq(0).addClass('active');
+            },
+            slideChangeTransitionStart: function () {
+                $('.swiper-progress-bar').removeClass('animate');
+                $('.swiper-progress-bar').removeClass('active');
+                $('.swiper-progress-bar').eq(0).addClass('active');
+            },
+            slideChangeTransitionEnd: function () {
+                $('.swiper-progress-bar').eq(0).addClass('animate');
+            },
+        }
+    });
+    
+
+
+    $('.home_slide').slick({
+        infinite: true,          
+        slidesToShow: 3,
+        arrows: true,
+        centerMode: true,
+        centerPadding : '220px',
+        responsive: [
+            // {
+            //     breakpoint: 768,
+            //     settings: {
+            //         arrows: false,
+            //         centerMode: true,
+            //         centerPadding: '40px',
+            //         slidesToShow: 3
+            //     }
+            // },
+            // {
+            //     breakpoint: 480,
+            //     settings: {
+            //         arrows: false,
+            //         centerMode: true,
+            //         centerPadding: '40px',
+            //         slidesToShow: 1
+            //     }
+            // }
+        ]
+    });
+
+
+    $(window).scroll(function () {
+        var scrT = $(window).scrollTop();
+        (scrT == $(document).height() - $(window).height())
+        ? $(".footer").addClass("active")
+        : $(".footer").removeClass("active")
+    });
+
+
+
+
+
+    // script_END
+})

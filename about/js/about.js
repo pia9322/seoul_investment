62603d96442cdfe11811eaa6a89ce8a43c6f5519
pia9322 <77098879+pia9322@users.noreply.github.com>
2021-08-03 18:08:34 +0900

    about_page add
    index.html
    about.html
    about_detail_01
    about_detail_02 (ing)

--- about/js/about.js
+++ about/js/about.js
@@ -4,6 +4,13 @@ $(function(){
     console.log(map_pointer);
 
 
+
+    // $('header').load('inc.html header nav', init)
+    // $('footer').load('inc.html footer figure')
+
+    // function init() {
+    // }
+
     for (let i=0; i < map_svg.length ; i++){
         $('.map_svg').eq(i).on('mouseover',function(){
             $('.point').eq(i).addClass('active');
@@ -11,5 +18,14 @@ $(function(){
         $('.map_svg').eq(i).on('mouseleave',function(){
             $('.point').eq(i).removeClass('active');
         }); 
+    }
+    
+    for (let j=0; j < map_svg.length ; j++){
+        $('.point').eq(j).on('mouseover',function(){
+            $('.map_svg').eq(j).addClass('active');
+        });
+        $('.point').eq(j).on('mouseleave',function(){
+            $('.map_svg').eq(j).removeClass('active');
+        });
     }
 });

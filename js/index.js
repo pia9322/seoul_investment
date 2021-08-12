56079da7af173cc56fc6a69efb8edc654898511b
pia9322 <77098879+pia9322@users.noreply.github.com>
2021-08-12 17:32:29 +0900

    ver 1.0.8 remaster
    main remaster...
    responsive modify comming soonnnn... -0-...

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ $(function(){
     AOS.init();
 
     
-    var swiper = new Swiper('.swiper-container', {
+    var swiper = new Swiper('.swiper_01', {
         effect: 'fade',
         autoplay: {
             delay: 5000,
@@ -34,45 +34,80 @@ $(function(){
             },
         }
     });
+   
+
+    $(function () {
+        $('.home_box').hover(function () {
+            $('.text_hover_box_02', this).stop().slideDown(150)
+        }, function () {
+            $('.text_hover_box_02', this).stop().slideUp(150)
+        })
+    });
+
+
+    // $(function () {
+    //     $('.home_box').hover(function () {
+    //         $(".home_detail").stop().slideDown(150)
+    //     }, function () {
+    //         $(".home_detail").stop().slideUp(150)
+    //     })
+    // });
+    
+    var swiper = new Swiper(".swiper_02", {
+        slidesPerView: 2.5,
+        spaceBetween: 20,
+        loop : true,
+        loopAdditionalSlides : 10,
+        // centeredSlides: true,
+        // navigation: {   // 버튼 사용자 지정
+        //     nextEl: '.swiper-button-next',
+        //     prevEl: '.swiper-button-prev',
+        // },
+        breakpoints : { // 반응형 설정이 가능 width값으로 조정
+            768 : {
+              slidesPerView : 1,
+            },
+        },
+    });
     
 
 
-    $('.home_slide').slick({
-        infinite: true,          
-        slidesToShow: 2,
-        arrows: true,
-        centerMode: true,
-        centerPadding : '220px',
-        responsive: [
-            {
-                breakpoint: 1440,
-                settings: {
-                    arrows: true,
-                    centerMode: true,
-                    centerPadding: '40px',
-                    slidesToShow: 2
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    arrows: true,
-                    centerMode: true,
-                    centerPadding: '40px',
-                    slidesToShow: 2
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    arrows: true,
-                    centerMode: true,
-                    centerPadding: '40px',
-                    slidesToShow: 1
-                }
-            }
-        ]
-    });
+    // $('.home_slide').slick({
+    //     infinite: true,          
+    //     slidesToShow: 2.1,
+    //     arrows: true,
+    //     // centerMode: true,
+    //     // centerPadding : '220px',
+    //     responsive: [
+    //         {
+    //             breakpoint: 1440,
+    //             settings: {
+    //                 arrows: true,
+    //                 // centerMode: true,
+    //                 // centerPadding: '40px',
+    //                 slidesToShow: 2
+    //             }
+    //         },
+    //         {
+    //             breakpoint: 768,
+    //             settings: {
+    //                 arrows: true,
+    //                 // centerMode: true,
+    //                 // centerPadding: '40px',
+    //                 slidesToShow: 1
+    //             }
+    //         },
+    //         {
+    //             breakpoint: 480,
+    //             settings: {
+    //                 arrows: true,
+    //                 centerMode: true,
+    //                 centerPadding: '40px',
+    //                 slidesToShow: 1
+    //             }
+    //         }
+    //     ]
+    // });
 
 
     $(window).scroll(function () {

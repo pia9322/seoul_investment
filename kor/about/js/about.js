bc770a56270b98958bb757947d7f5f8f7cf39ece
pia9322 <77098879+pia9322@users.noreply.github.com>
2021-08-24 15:08:42 +0900

    reload patching

--- kor/about/js/about.js
+++ kor/about/js/about.js
@@ -8,19 +8,24 @@ $(function(){
     $('header').load('../include/init.html header .header_wrap', init);
     $('.footer').load('../include/init.html .footer > .footer_wrap');
 
-    function init() {};
+    function init() {
+        
+        function headFun(){
+            let href = location.href;
+            let headerNav = document.querySelector('.header_navi');
 
-    // function headFun(){
-    //     let href = location.href;
-    //     let headerNav = document.querySelector('.header_navi');
+            if (href.indexOf("main") <= 0) {
+                headerNav.classList.add("active")
+            } else {
+                headerNav.classList.remove("active")
+            }
+        }
+        headFun()
+        // console.log($('.big_category').text())
+        $('.big_category').text($('#now_place > p:eq(0)').text())
+        $('.small_category').text($('#now_place > p:eq(1)').text())
+    };
 
-    //     if (href.indexOf("index.html") <= 0) {
-    //         headerNav.classList.add("active")
-    //     } else {
-    //         headerNav.classList.remove("active")
-    //     }
-    // }
-    // headFun()
 
     for (let i=0; i < map_svg.length ; i++){
         $('.map_svg').eq(i).on('mouseover',function(){
